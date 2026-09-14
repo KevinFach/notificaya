7f7a195c8a25149781aff8ad89e038ab0c7f85f2
Kevin Chi <kevinfabian2911@gmail.com>
2026-09-14 09:47:05 -0600

    Ocultar temporalmente el recurso de Reglas de Notificación

--- app/Filament/Resources/NotificationRules/NotificationRuleResource.php
+++ app/Filament/Resources/NotificationRules/NotificationRuleResource.php
@@ -19,6 +19,8 @@ class NotificationRuleResource extends Resource
 {
     protected static ?string $model = NotificationRule::class;
 
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBellAlert;
 
     protected static string|UnitEnum|null $navigationGroup = 'Configuración';
